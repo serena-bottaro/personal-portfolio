a650a9bc6f036213fc98851c962b7f85a8bb5a83
Serena-Bottaro <84742375+serena-bottaro@users.noreply.github.com>
2022-10-24 19:06:03 +0200

    fixed js
    nav

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 
-      src="https://kit.fontawesome.com/1f25bafa85.js"
-      crossorigin="anonymous"
+src="https://kit.fontawesome.com/1f25bafa85.js"
+crossorigin="anonymous";
     
     
-      function scrollToTop() {
-        window.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: "smooth",
-        });
-      }
-    
-      const hamburger = document.querySelector(".hamburger");
-      const navList = document.querySelector(".nav-list");
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
 
-      const checkbox = document.querySelector("input");
-      const links = document.querySelectorAll(".nav-list a");
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
 
-      hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("active");
-        navList.classList.toggle("active");
-      });
+const checkbox = document.querySelector("input");
+const links = document.querySelectorAll(".nav-list a");
 
-      document.querySelectorAll(".nav-link").forEach((n) =>
-        n.addEventListener("click", () => {
-          hamburger.classList.remove("active");
-          navList.classList.remove("active");
-        })
-      );
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navList.classList.toggle("active");
+});
 
-      for (const link of links) {
-        link.onclick = handleClick;
-      }
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
+  })
+);
 
-      function handleClick() {
-        checkbox.checked = false;
-      }
+for (const link of links) {
+  link.onclick = handleClick;
+};
+
+function handleClick() {
+  checkbox.checked = false;
+};
    
